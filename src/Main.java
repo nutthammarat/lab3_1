0e66ee7f8ea36e6a9411572efb9db99f250296a1
earthntp <reventon11@outlook.com>
2018-08-31 08:21:59 +0700

    Lab3-1 first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("ใส่ ตัวเลข 1 ตัว ", "0");
+        JOptionPane.showMessageDialog(null,input,"title",JOptionPane.INFORMATION_MESSAGE);
+        int ans = JOptionPane.showConfirmDialog(null,"รักหรือเปล่า","ถามเธอ",JOptionPane.YES_NO_OPTION);
+    if(ans == JOptionPane.YES_OPTION){
+        JOptionPane.showConfirmDialog(null,"รักนะจุ๊บๆ");
+    }else{
+        JOptionPane.showConfirmDialog(null,"จำไว้เลย");
+    }
+
+
+    }
+
+}
